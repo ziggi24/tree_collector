f2ed118f70a1d3159377c6b1afbade657afb3ac0
Zach <ziggi24@gmail.com>
2020-06-17 10:41:24 -0700

    added user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -15,6 +16,8 @@ class Tree(models.Model):
   height = models.CharField(max_length=100)
   species = models.CharField(max_length=100)
   workers = models.ManyToManyField(Worker)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
 
   def __str__(self):
     return f"{self.name} the {self.species}"
